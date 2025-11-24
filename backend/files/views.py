7aafc38e856583288508ad9e6f54bfc1d5f123d3
HurmainFatima <149819596+HurmainFatima@users.noreply.github.com>
2025-11-24 21:02:25 +0500

    Remove unused import from views.py
    Removed unused import of 'status' from rest_framework.

--- backend/files/views.py
+++ backend/files/views.py
@@ -1,6 +1,5 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.parsers import JSONParser
 from django.http import FileResponse, HttpResponse
 from .models import EncryptedFile
